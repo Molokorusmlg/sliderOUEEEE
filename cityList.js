f800bffd627bd3b388b36af36da78bc2448c36a0
Mitia Yakor <147729819+Molokorusmlg@users.noreply.github.com>
2024-10-15 15:18:07 +0300

    Add files via upload

---
+++ cityList.js
@@ -0,0 +1,57 @@
+// export const cityList = [
+//   {
+//     Img_scr: "./assets/img/1905year.jpg",
+//     Map_scr:
+//       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2182.4491093621905!2d60.59446207744233!3d56.83823997350532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c16e8ab92d9b9f%3A0xa9573fbeb44831e9!2z0L_Quy4gMTkwNSDQs9C-0LTQsCwg0JXQutCw0YLQtdGA0LjQvdCx0YPRgNCzLCDQodCy0LXRgNC00LvQvtCy0YHQutCw0Y8g0L7QsdC7LiwgNjIwMDE0!5e0!3m2!1sru!2sru!4v1728936782649!5m2!1sru!2sru",
+//     Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, ratione quas beatae ex, error laudantium fugit necessitatibus magni ea neque est molestias iste iure voluptates.",
+//   },
+//   {
+//     Img_scr: "./assets/img/1905year.jpg",
+//     Map_scr:
+//       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2182.4491093621905!2d60.59446207744233!3d56.83823997350532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c16e8ab92d9b9f%3A0xa9573fbeb44831e9!2z0L_Quy4gMTkwNSDQs9C-0LTQsCwg0JXQutCw0YLQtdGA0LjQvdCx0YPRgNCzLCDQodCy0LXRgNC00LvQvtCy0YHQutCw0Y8g0L7QsdC7LiwgNjIwMDE0!5e0!3m2!1sru!2sru!4v1728936782649!5m2!1sru!2sru",
+//     Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, ratione quas beatae ex, error laudantium fugit necessitatibus magni ea neque est molestias iste iure voluptates.",
+//   },
+//   {
+//     Img_scr: "./assets/img/1905year.jpg",
+//     Map_scr:
+//       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2182.4491093621905!2d60.59446207744233!3d56.83823997350532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c16e8ab92d9b9f%3A0xa9573fbeb44831e9!2z0L_Quy4gMTkwNSDQs9C-0LTQsCwg0JXQutCw0YLQtdGA0LjQvdCx0YPRgNCzLCDQodCy0LXRgNC00LvQvtCy0YHQutCw0Y8g0L7QsdC7LiwgNjIwMDE0!5e0!3m2!1sru!2sru!4v1728936782649!5m2!1sru!2sru",
+//     Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, ratione quas beatae ex, error laudantium fugit necessitatibus magni ea neque est molestias iste iure voluptates.",
+//   },
+//   {
+//     Img_scr: "./assets/img/1905year.jpg",
+//     Map_scr:
+//       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2182.4491093621905!2d60.59446207744233!3d56.83823997350532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c16e8ab92d9b9f%3A0xa9573fbeb44831e9!2z0L_Quy4gMTkwNSDQs9C-0LTQsCwg0JXQutCw0YLQtdGA0LjQvdCx0YPRgNCzLCDQodCy0LXRgNC00LvQvtCy0YHQutCw0Y8g0L7QsdC7LiwgNjIwMDE0!5e0!3m2!1sru!2sru!4v1728936782649!5m2!1sru!2sru",
+//     Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, ratione quas beatae ex, error laudantium fugit necessitatibus magni ea neque est molestias iste iure voluptates.",
+//   },
+//   {
+//     Img_scr: "./assets/img/1905year.jpg",
+//     Map_scr:
+//       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2182.4491093621905!2d60.59446207744233!3d56.83823997350532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c16e8ab92d9b9f%3A0xa9573fbeb44831e9!2z0L_Quy4gMTkwNSDQs9C-0LTQsCwg0JXQutCw0YLQtdGA0LjQvdCx0YPRgNCzLCDQodCy0LXRgNC00LvQvtCy0YHQutCw0Y8g0L7QsdC7LiwgNjIwMDE0!5e0!3m2!1sru!2sru!4v1728936782649!5m2!1sru!2sru",
+//     Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, ratione quas beatae ex, error laudantium fugit necessitatibus magni ea neque est molestias iste iure voluptates.",
+//   },
+//   {
+//     Img_scr: "./assets/img/1905year.jpg",
+//     Map_scr:
+//       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2182.4491093621905!2d60.59446207744233!3d56.83823997350532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c16e8ab92d9b9f%3A0xa9573fbeb44831e9!2z0L_Quy4gMTkwNSDQs9C-0LTQsCwg0JXQutCw0YLQtdGA0LjQvdCx0YPRgNCzLCDQodCy0LXRgNC00LvQvtCy0YHQutCw0Y8g0L7QsdC7LiwgNjIwMDE0!5e0!3m2!1sru!2sru!4v1728936782649!5m2!1sru!2sru",
+//     Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, ratione quas beatae ex, error laudantium fugit necessitatibus magni ea neque est molestias iste iure voluptates.",
+//   },
+//   {
+//     Img_scr: "./assets/img/1905year.jpg",
+//     Map_scr:
+//       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2182.4491093621905!2d60.59446207744233!3d56.83823997350532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c16e8ab92d9b9f%3A0xa9573fbeb44831e9!2z0L_Quy4gMTkwNSDQs9C-0LTQsCwg0JXQutCw0YLQtdGA0LjQvdCx0YPRgNCzLCDQodCy0LXRgNC00LvQvtCy0YHQutCw0Y8g0L7QsdC7LiwgNjIwMDE0!5e0!3m2!1sru!2sru!4v1728936782649!5m2!1sru!2sru",
+//     Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, ratione quas beatae ex, error laudantium fugit necessitatibus magni ea neque est molestias iste iure voluptates.",
+//   },
+//   {
+//     Img_scr: "./assets/img/1905year.jpg",
+//     Map_scr:
+//       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2182.4491093621905!2d60.59446207744233!3d56.83823997350532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c16e8ab92d9b9f%3A0xa9573fbeb44831e9!2z0L_Quy4gMTkwNSDQs9C-0LTQsCwg0JXQutCw0YLQtdGA0LjQvdCx0YPRgNCzLCDQodCy0LXRgNC00LvQvtCy0YHQutCw0Y8g0L7QsdC7LiwgNjIwMDE0!5e0!3m2!1sru!2sru!4v1728936782649!5m2!1sru!2sru",
+//     Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, ratione quas beatae ex, error laudantium fugit necessitatibus magni ea neque est molestias iste iure voluptates.",
+//   },
+//   {
+//     Img_scr: "./assets/img/1905year.jpg",
+//     Map_scr:
+//       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2182.4491093621905!2d60.59446207744233!3d56.83823997350532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c16e8ab92d9b9f%3A0xa9573fbeb44831e9!2z0L_Quy4gMTkwNSDQs9C-0LTQsCwg0JXQutCw0YLQtdGA0LjQvdCx0YPRgNCzLCDQodCy0LXRgNC00LvQvtCy0YHQutCw0Y8g0L7QsdC7LiwgNjIwMDE0!5e0!3m2!1sru!2sru!4v1728936782649!5m2!1sru!2sru",
+//     Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, ratione quas beatae ex, error laudantium fugit necessitatibus magni ea neque est molestias iste iure voluptates.",
+//   },
+// ];
+
---
+++ script.js
@@ -0,0 +1,121 @@
+const cityList = [
+  {
+    Img_scr: "./assets/img/1905year.jpg",
+    Map_scr:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2182.4491093621905!2d60.59446207744233!3d56.83823997350532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c16e8ab92d9b9f%3A0xa9573fbeb44831e9!2z0L_Quy4gMTkwNSDQs9C-0LTQsCwg0JXQutCw0YLQtdGA0LjQvdCx0YPRgNCzLCDQodCy0LXRgNC00LvQvtCy0YHQutCw0Y8g0L7QsdC7LiwgNjIwMDE0!5e0!3m2!1sru!2sru!4v1728936782649!5m2!1sru!2sru",
+    Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, ratione quas beatae ex, error laudantium fugit necessitatibus magni ea neque est molestias iste iure voluptates.",
+  },
+  {
+    Img_scr: "./assets/img/1905year.jpg",
+    Map_scr:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2182.4491093621905!2d60.59446207744233!3d56.83823997350532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c16e8ab92d9b9f%3A0xa9573fbeb44831e9!2z0L_Quy4gMTkwNSDQs9C-0LTQsCwg0JXQutCw0YLQtdGA0LjQvdCx0YPRgNCzLCDQodCy0LXRgNC00LvQvtCy0YHQutCw0Y8g0L7QsdC7LiwgNjIwMDE0!5e0!3m2!1sru!2sru!4v1728936782649!5m2!1sru!2sru",
+    Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, ratione quas beatae ex, error laudantium fugit necessitatibus magni ea neque est molestias iste iure voluptates.",
+  },
+  {
+    Img_scr: "./assets/img/1905year.jpg",
+    Map_scr:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2182.4491093621905!2d60.59446207744233!3d56.83823997350532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c16e8ab92d9b9f%3A0xa9573fbeb44831e9!2z0L_Quy4gMTkwNSDQs9C-0LTQsCwg0JXQutCw0YLQtdGA0LjQvdCx0YPRgNCzLCDQodCy0LXRgNC00LvQvtCy0YHQutCw0Y8g0L7QsdC7LiwgNjIwMDE0!5e0!3m2!1sru!2sru!4v1728936782649!5m2!1sru!2sru",
+    Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, ratione quas beatae ex, error laudantium fugit necessitatibus magni ea neque est molestias iste iure voluptates.",
+  },
+  {
+    Img_scr: "./assets/img/1905year.jpg",
+    Map_scr:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2182.4491093621905!2d60.59446207744233!3d56.83823997350532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c16e8ab92d9b9f%3A0xa9573fbeb44831e9!2z0L_Quy4gMTkwNSDQs9C-0LTQsCwg0JXQutCw0YLQtdGA0LjQvdCx0YPRgNCzLCDQodCy0LXRgNC00LvQvtCy0YHQutCw0Y8g0L7QsdC7LiwgNjIwMDE0!5e0!3m2!1sru!2sru!4v1728936782649!5m2!1sru!2sru",
+    Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, ratione quas beatae ex, error laudantium fugit necessitatibus magni ea neque est molestias iste iure voluptates.",
+  },
+  {
+    Img_scr: "./assets/img/1905year.jpg",
+    Map_scr:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2182.4491093621905!2d60.59446207744233!3d56.83823997350532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c16e8ab92d9b9f%3A0xa9573fbeb44831e9!2z0L_Quy4gMTkwNSDQs9C-0LTQsCwg0JXQutCw0YLQtdGA0LjQvdCx0YPRgNCzLCDQodCy0LXRgNC00LvQvtCy0YHQutCw0Y8g0L7QsdC7LiwgNjIwMDE0!5e0!3m2!1sru!2sru!4v1728936782649!5m2!1sru!2sru",
+    Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, ratione quas beatae ex, error laudantium fugit necessitatibus magni ea neque est molestias iste iure voluptates.",
+  },
+  {
+    Img_scr: "./assets/img/1905year.jpg",
+    Map_scr:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2182.4491093621905!2d60.59446207744233!3d56.83823997350532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c16e8ab92d9b9f%3A0xa9573fbeb44831e9!2z0L_Quy4gMTkwNSDQs9C-0LTQsCwg0JXQutCw0YLQtdGA0LjQvdCx0YPRgNCzLCDQodCy0LXRgNC00LvQvtCy0YHQutCw0Y8g0L7QsdC7LiwgNjIwMDE0!5e0!3m2!1sru!2sru!4v1728936782649!5m2!1sru!2sru",
+    Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, ratione quas beatae ex, error laudantium fugit necessitatibus magni ea neque est molestias iste iure voluptates.",
+  },
+  {
+    Img_scr: "./assets/img/1905year.jpg",
+    Map_scr:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2182.4491093621905!2d60.59446207744233!3d56.83823997350532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c16e8ab92d9b9f%3A0xa9573fbeb44831e9!2z0L_Quy4gMTkwNSDQs9C-0LTQsCwg0JXQutCw0YLQtdGA0LjQvdCx0YPRgNCzLCDQodCy0LXRgNC00LvQvtCy0YHQutCw0Y8g0L7QsdC7LiwgNjIwMDE0!5e0!3m2!1sru!2sru!4v1728936782649!5m2!1sru!2sru",
+    Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, ratione quas beatae ex, error laudantium fugit necessitatibus magni ea neque est molestias iste iure voluptates.",
+  },
+  {
+    Img_scr: "./assets/img/1905year.jpg",
+    Map_scr:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2182.4491093621905!2d60.59446207744233!3d56.83823997350532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c16e8ab92d9b9f%3A0xa9573fbeb44831e9!2z0L_Quy4gMTkwNSDQs9C-0LTQsCwg0JXQutCw0YLQtdGA0LjQvdCx0YPRgNCzLCDQodCy0LXRgNC00LvQvtCy0YHQutCw0Y8g0L7QsdC7LiwgNjIwMDE0!5e0!3m2!1sru!2sru!4v1728936782649!5m2!1sru!2sru",
+    Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, ratione quas beatae ex, error laudantium fugit necessitatibus magni ea neque est molestias iste iure voluptates.",
+  },
+  {
+    Img_scr: "./assets/img/1905year.jpg",
+    Map_scr:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2182.4491093621905!2d60.59446207744233!3d56.83823997350532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c16e8ab92d9b9f%3A0xa9573fbeb44831e9!2z0L_Quy4gMTkwNSDQs9C-0LTQsCwg0JXQutCw0YLQtdGA0LjQvdCx0YPRgNCzLCDQodCy0LXRgNC00LvQvtCy0YHQutCw0Y8g0L7QsdC7LiwgNjIwMDE0!5e0!3m2!1sru!2sru!4v1728936782649!5m2!1sru!2sru",
+    Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, ratione quas beatae ex, error laudantium fugit necessitatibus magni ea neque est molestias iste iure voluptates.",
+  },
+];
+
+
+function cikle() {
+  const parentCards = document.querySelector(
+    ".redline__marshruts__cardBox_SmallCardBox"
+  );
+  cityList.forEach((city) => {
+    const card = document
+      .createElement("div")
+    card.classList.add("redline__marshruts__cardBox_card");
+    card.innerHTML = `<div class="redline__marshruts__cardBox_card-img">
+                  <img src=${city.Img_scr} alt="" />
+                  
+                 </div>
+                <div class="redline__marshruts__cardBox_card-columnbox">
+                  <div class="redline__marshruts__cardBox_card-map">
+                    <iframe
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d458.7735707547828!2d60.6211135126979!3d56.840674693157965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c16e58e6df1121%3A0xb4486da0a3cefcd8!2z0JPQvtGB0YLQuNC90LjRhtCwINCY0YHQtdGC0Yw!5e0!3m2!1sru!2sru!4v1728981840534!5m2!1sru!2sru"
+                      width="200"
+                      height="200"
+                      style="border: 0;border-radius: 5px"
+                      allowfullscreen=""
+                      loading="lazy"
+                      referrerpolicy="no-referrer-when-downgrade"
+                    ></iframe>
+                  </div>
+                  <div class="redline__marshruts__cardBox_card-description">
+                    <p>
+                      ${city.Text}
+                    </p>
+                  </div>
+              </div>`;
+    parentCards.appendChild(card);
+  });
+}
+cikle();
+
+const cards = document.querySelectorAll(".redline__marshruts__cardBox_card");
+let curentCardIndex = 0;
+
+function ShowCard(index) {
+  cards[curentCardIndex].classList.remove("active");
+  cards[index].classList.add("active");
+  curentCardIndex = index;
+  const active_title_text = document.getElementById(index);
+  const title_blockd = document.querySelector(".redline__marshruts-text");
+}
+
+document.querySelector(".controls").addEventListener("click", function (event) {
+  if (event.target.classList.contains("arrow")) {
+    let index = curentCardIndex - 1;
+    if (index < 0) {
+      index = cards.length - 1;
+    }
+    ShowCard(index);
+  } else if (event.target.classList.contains("arrow-reverse")) {
+    let index = curentCardIndex + 1;
+    if (index >= cards.length) {
+      index = 0;
+    }
+    ShowCard(index);
+  }
+});
+ShowCard(curentCardIndex);
+
